e03c8b1a46d1175db9970c441dedac56f30869b7
Cody Hussey <3787405e@opayq.com>
2019-03-25 16:17:38 -0400

    logging added

--- scripts/install_chocolatey.ps1
+++ scripts/install_chocolatey.ps1
@@ -1,2 +1,6 @@
+# Install Chocolately
 $env:chocolateyUseWindowsCompression = 'false'
 Invoke-WebRequest -Uri 'https://chocolatey.org/install.ps1' -UseBasicParsing | Invoke-Expression
+
+# Log
+Write-Output "$(Get-TimeStamp) Packer installed Chocolatey package manager. Yay!" | Out-File C:\packerlog.txt -Append
